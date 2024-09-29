ea5b253ad0a69f5afae0dd5b31a07e9485f9e9de
jimmyboy722 <jeckman722@gmail.com>
2024-09-29 12:43:15 -0400

    added notes to the seeds' index.js file explaining functionality

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,25 +1,29 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+// IMPORTING ALL THE SEEDS
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
+// CONNECTING TO THE DATABASE
+const sequelize = require("../config/connection");
 
-const sequelize = require('../config/connection');
-
+// ASYNCHRONOUS FUNCTION THAT SEEDS THE DATABASE WITH PRODUCTS, CATEGORIES, TAGS AND PRODUCT TAGS
+// THEN LOGS THAT THE DATABASE HAS BEEN SYNCED AND CATEGORIES, PRODUCTS, TAGS AND PRODUCT TAGS HAVE BEEN SEEDED
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\n----- DATABASE SYNCED -----\n");
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log("\n----- CATEGORIES SEEDED -----\n");
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log("\n----- PRODUCTS SEEDED -----\n");
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log("\n----- TAGS SEEDED -----\n");
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  console.log("\n----- PRODUCT TAGS SEEDED -----\n");
 
+  // EXITS WITH A SUCCESS CODE OF 0
   process.exit(0);
 };
 
