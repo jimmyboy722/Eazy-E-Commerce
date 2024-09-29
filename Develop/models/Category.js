3485a42ae8ca3ee5bc6eb93bf3757090a5f5fc8a
jimmyboy722 <jeckman722@gmail.com>
2024-09-28 20:42:08 -0400

    added columns and notes to the category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+// MODEL IS THE BASE CLASS FOR ALL MODELS AND DATATYPES PROVIDE THE VARIOUS DATA TYPES LIKE 'INTEGER', 'STRING', ETC...
+const { Model, DataTypes } = require("sequelize");
+// LINE 2 IMPORTS THE SEQUELIZE INSTANCE AND LINE 4 CONNECTS TO THE DATABASE
+const sequelize = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
-
+// DEFINING THE CATEGORY CLASS AND EXTENDS THE MODEL CLASS GIVEN BY SEQUELIZE. CATEGORY WILL INHERIT FROM THE MODEL
 class Category extends Model {}
 
+// INITIALIZING THE CATEGORY CLASS, DEFINING THE MODEL'S ATTRIBUTES AND VALUES
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, // PREVENTS NULL VALUES
+      primaryKey: true, // TELLS SEQUELIZE THAT THIS IS THE PRIMARY KEY
+      autoIncrement: true, // THE VALUE OF ID WILL INCREMENT WITH EVERY NEW ENTRY
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+    // SPECIFYING THE INSTANCE OF SEQUELIZE TO USE
     sequelize,
     timestamps: false,
+    // PREVENTS SEQUELIZE FROM REPLACING TABLE NAME
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
