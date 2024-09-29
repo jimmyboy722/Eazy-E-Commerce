f622e4366bfd41cd830e24ee01221f0bb2605774
jimmyboy722 <jeckman722@gmail.com>
2024-09-29 12:23:46 -0400

    added attributes and notes to the product model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,19 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // ATTRIBUTES
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // ESTABLISHES A FOREIGN KEY RELATIONSHIP WITH THE PRODUCT MODEL/TABLE
+        model: "product",
+        key: "id", // SHOWS LINK TO THE ID COLUMN IN THE PRODUCT TABLE
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id", // ALSO SHOWS LINK TO THE ID COLUMN IN THE TAG TABLE
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
