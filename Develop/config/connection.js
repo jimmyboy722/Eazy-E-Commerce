efa14e7ac2a1a7389a1fad2ac699072134592d72
jimmyboy722 <jeckman722@gmail.com>
2024-09-28 20:21:32 -0400

    ran npm i for dependencies and added notes to connection file

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
+//IMPORTING SEQUELIZE
+const Sequelize = require("sequelize");
+// CONNECTING TO ENVIRONMENT VARIABLES
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
-
+// USING THOSE ENVIRONMENT VARIABLES TO CONNECT TO DATABASE
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
+// EXPORTING SEQUELIZE
 module.exports = sequelize;
