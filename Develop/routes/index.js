8a7b1b4b900f7dbf90b0d51ff7c79511a4961f8e
jimmyboy722 <jeckman722@gmail.com>
2024-10-02 12:29:18 -0400

    debugging dpath errors

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,13 +1,9 @@
 // IMPORTING THE ROUTER CLASS AND THE ROUTE MODULES
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const apiRoutes = require("./api/index");
 
-// MOUNTING THE ROUTES TO THE CORRESPONDING PATHS. I.E. ANY REQUESTS TO /CATEGORIES WILL BE HANDLED BY THE CATEGORY ROUTE
-router.use('/categories', categoryRoutes)
-router.use('/products' productRoutes)
-router.use('/tags', tagRoutes);
+// MOUNTING THE ROUTES
+router.use("/api", apiRoutes);
 
 // EXPORTING THE ROUTER
 module.exports = router;
